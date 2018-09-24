6367e3ff4f7ff842d52f82ac3e7f29c88ecfb90f
yangjin <kingyouge@gmail.com>
2018-09-24 22:47:06 +0800

    删除注释掉的代码

--- src/components/home/slidebar.vue
+++ src/components/home/slidebar.vue
@@ -10,18 +10,6 @@
       <div class="slideBar__expand"  @click="isCollapse = !isCollapse">
         <i :class="['iconfont', isCollapse ? 'icon-zhankaicelalan' : 'icon-shouhuicelalan']"></i>
       </div>
-      <!-- <el-menu-item index="2">
-        <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
-      </el-menu-item>
-      <el-menu-item index="3" disabled>
-        <i class="el-icon-document"></i>
-        <span slot="title">导航三</span>
-      </el-menu-item>
-      <el-menu-item index="4">
-        <i class="el-icon-setting"></i>
-        <span slot="title">导航四</span>
-      </el-menu-item> -->
       <slidebar-menu v-for="item in menu" :key="item.path" :data="item"></slidebar-menu>
     </el-menu>
   </div>
