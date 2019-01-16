6086de94b03aa0285278e0e5d1961cd91cd2a8a9
yangjin <kingyouge@gmail.com>
2019-01-16 16:18:36 +0800

    test: test tree-shaking

---
+++ src/components/test/test.js
@@ -0,0 +1,6 @@
+import Test1 from './test1.vue'
+import Test2 from './test2.vue'
+export {
+  Test1,
+  Test2
+}
--- src/views/Loading.vue
+++ src/views/Loading.vue
@@ -1,10 +1,14 @@
 <template>
 <div class="content__main">
-  xxxxx
+  <test1></test1>
 </div>
 </template>
 <script>
+import { Test1 } from './../components/test/test.js'
 export default {
+  components: {
+    Test1
+  },
   data() {
     return {
       
