1a5749e6e76b68b06bfd46eeda688513aa085e01
yangjin <kingyouge@gmail.com>
2019-03-13 23:36:43 +0800

    modify: delete useless content

--- src/views/Loading.vue
+++ src/views/Loading.vue
@@ -1,14 +1,9 @@
 <template>
 <div class="content__main">
-  <test1></test1>
 </div>
 </template>
 <script>
-import { Test1 } from './../components/test/test.js'
 export default {
-  components: {
-    Test1
-  },
   data() {
     return {
       
