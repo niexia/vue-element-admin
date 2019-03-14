6d871691e1557cc1d51f1ebb7bbf04157420b917
yangjin <kingyouge@gmail.com>
2019-03-14 10:31:41 +0800

    modify: adjust header layout

--- src/components/home/header.vue
+++ src/components/home/header.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="content__header">
-    <p>组件学习</p>
-    <el-tooltip effect="dark" content="关于我(Github)" placement="left-start">
-      <div class="about">
-        <a href="https://github.com/yangseas" target="_blank"></a>
+    <div class="header__navbar">
+      <ul class="navbar__menu">
+        <li class="menu__item"><i class="icon-home p_r10"></i>首页</li>
+        <li class="menu__item"><i class="icon-email p_r10"></i>反馈</li>
+        <li class="menu__item"><i class="icon-question-mark-circle p_r10"></i>帮助</li>
+    </ul>
+    </div>
+    <el-dropdown class="header__user">
+      <div class="user__profile">
+        <img src="./../../assets/imgs/icon__logo.png">
+        <p>
+          <span style="font-size: 13px;">杨小文</span><br>
+          <span style="font-size: 12px;">技术部</span>
+        </p>
       </div>
-    </el-tooltip>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item><i class="icon-person"></i>个人中心</el-dropdown-item>
+        <el-dropdown-item><i class="icon-log-out"></i>退出登录</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
   </div>
 </template>
 <script>
@@ -21,17 +35,36 @@ export default {
   box-sizing: border-box;
   padding: 5px 20px;
 }
-.content__header .about{
+.content__header .header__navbar {
+  flex: 1;
+  color: #666;
+  font-size: 12px;
+}
+.content__header .header__navbar .navbar__menu {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+.content__header .header__navbar .navbar__menu .menu__item{
+  width: 90px;
+  text-align: center;
+}
+.content__header .header__user {
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+}
+.content__header .header__user .user__profile {
+  display: flex;
+  align-items: center;
+  margin-bottom: 2px;
+  text-align: center;
+  color: #333;
+}
+.content__header .header__user .user__profile img {
+  display: inline-block;
   height: 40px;
   width: 40px;
-  border-radius: 50%;
-  background: url("./../../assets/imgs/icon__logo.png") center no-repeat;
-  background-size: 100% 100%;
-  cursor: pointer;
-}
-.content__header .about a{
-  display: inline-block;
-  width: 100%;
-  height: 100%;
+  margin-right: 10px;
 }
 </style>
