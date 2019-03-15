8d7bbfdd9fbdd3b4390f8e0cd007b6b952374a43
yangjin <kingyouge@gmail.com>
2019-03-15 14:20:54 +0800

    modify: all pages add template

---
+++ src/views/exceptionPage/404.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script>
+export default {
+  name: '',
+  mixins: [],
+  components: {},
+  props: {},
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+    
+  },
+  watch: {
+    
+  },
+  created () {
+    
+  },
+  mounted() {
+    
+  },
+  destroyed() {
+    
+  },
+  methods: {
+    
+  }
+}
+</script>
+<style scoped>
+
+</style>
+
--- src/views/template.vue
+++ src/views/template.vue
@@ -1,32 +1,40 @@
 <template>
-  <div class="content__main">
-    <el-row>
-      <p><el-button type="text">效果</el-button></p>
-    </el-row>
-    <el-row>
-      <p><el-button type="text">核心代码</el-button></p>
-    </el-row>
+  <div>
+
   </div>
 </template>
 <script>
-  export default{
-    name: '',
-    mixins: [],
-    components: {},
-    props: {},
-    data(){
-      return{
-      }
-    },
-    computed: {},
-    watch: {},
-    created(){},
-    mounted(){},
-    destroyed(){},
-    methods: {
+export default {
+  name: '',
+  mixins: [],
+  components: {},
+  props: {},
+  data() {
+    return {
+
     }
+  },
+  computed: {
+    
+  },
+  watch: {
+    
+  },
+  created () {
+    
+  },
+  mounted() {
+    
+  },
+  destroyed() {
+    
+  },
+  methods: {
+    
   }
+}
 </script>
-<style>
+<style scoped>
+
 </style>
 
