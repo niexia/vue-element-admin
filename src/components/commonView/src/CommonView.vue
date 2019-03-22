efaa86762c23608e19c72a1e46072c1187845543
yangjin <kingyouge@gmail.com>
2019-03-22 15:08:25 +0800

    modify: complete the features of CommonView

--- src/components/commonView/src/CommonView.vue
+++ src/components/commonView/src/CommonView.vue
@@ -3,15 +3,40 @@
     <div class="viewBox">
       <header class="view__header">
         <slot name="header">
-          header
+          <section class="header__search">
+            <slot name="search">
+              <el-form :inline="true" :model="searchParams" ref="searchForm">
+                <el-form-item label="姓名" prop="param1">
+                  <el-input v-model="searchParams.param1" placeholder="请输入姓名"></el-input>
+                </el-form-item>
+                <el-form-item label="状态" prop="param2">
+                  <el-select v-model="searchParams.param2" placeholder="请选择状态">
+                    <el-option label="成功" value="shanghai"></el-option>
+                    <el-option label="失败" value="beijing"></el-option>
+                  </el-select>
+                </el-form-item>
+                <el-form-item>
+                  <el-button type="primary" @click="handleSearch">查询</el-button>
+                  <el-button type="primary" @click="handleReset">重置</el-button>
+                </el-form-item>
+              </el-form>
+            </slot>
+          </section>
+          <section class="header__operate">
+            <slot name="operate">
+              <el-button type="primary">新增</el-button>
+              <el-button type="danger">删除</el-button>
+              <el-button>导出</el-button>
+            </slot>
+          </section>
         </slot>
       </header>
       <main class="view__main">
         <slot name="main">
           <el-table
-            :data="tableData3"
-            border
+            :data="tableData"
             :height="tableHeight"
+            border
             style="width: 100%">
             <el-table-column
               prop="date"
@@ -35,7 +60,7 @@
           <el-pagination
             @size-change="handleSizeChange"
             @current-change="handleCurrentChange"
-            :current-page="currentPage4"
+            :current-page="pageSize"
             :page-sizes="[100, 200, 300, 400]"
             :page-size="100"
             layout="total, sizes, prev, pager, next, jumper"
@@ -58,12 +83,16 @@ export default {
     },
     isFixedHeader: { // fixed table header
       type: Boolean,
-      default: false
+      default: true
     }
   },
   data() {
     return {
-      tableData3: [{
+      searchParams: {
+        param1: '',
+        param2: ''
+      },
+      tableData: [{
         date: '2016-05-03',
         name: '王小虎',
         address: '上海市普陀区金沙江路 1518 弄'
@@ -85,98 +114,10 @@ export default {
         address: '上海市普陀区金沙江路 1518 弄'
       }, {
         date: '2016-05-06',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-07',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-08',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-06',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-07',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-08',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-06',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-07',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-08',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-06',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-07',
         name: '王小虎',
         address: '上海市普陀区金沙江路 1518 弄'
       }],
-      currentPage4: 1
+      pageSize: 1
     }
   },
   computed: {
@@ -193,15 +134,22 @@ export default {
   destroyed() {},
   methods: {
     handleSizeChange(val) {
-      console.log(`每页 ${val} 条`);
+      this.$emit('size-change');
     },
     handleCurrentChange(val) {
-      console.log(`当前页: ${val}`);
+      this.$emit('current-change');
+    },
+    handleSearch() {
+      this.$emit('search');
+    },
+    handleReset() {
+      this.$refs.searchForm.resetFields();
+      this.$emit('reset');
     }
   }
 }
 </script>
-<style scoped>
+<style>
 /* fixed table header */
 .main__view.is-fixed {
   height: 100%;
@@ -223,7 +171,13 @@ export default {
   background: #fff;
 }
 .main__view .view__header {
-  padding-bottom: 8px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: 20px;
+}
+.main__view .view__header .el-form .el-form-item {
+  margin-bottom: 0;
 }
 .main__view .view__main {
   flex: 1;
@@ -232,6 +186,14 @@ export default {
   height: 40px;
   padding-top: 8px;
   box-sizing: border-box;
+}
+.main__view .view__footer .el-pagination {
+  position: relative;
+  text-align: right;
+}
+.main__view .view__footer .el-pagination .el-pagination__total {
+  position: absolute;
+  left: 0;
 }
 </style>
 
