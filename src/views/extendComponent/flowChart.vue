784c6420fe7875c43a243190c5213327fea7ac55
yangjin <kingyouge@gmail.com>
2019-12-04 13:35:36 +0800

    modify: 未完成的页面用 code 组件显示

--- src/views/extendComponent/flowChart.vue
+++ src/views/extendComponent/flowChart.vue
@@ -1,37 +1,7 @@
 <template>
-  <div>
-    <el-button @click="handleAddData">添加数据</el-button>
-    <el-button @click="handleDeleteData">删除数据</el-button>
-    <div style="margin: 20px;">
-      <nx-transfer filterable v-model="value" :data="data"></nx-transfer>
-    </div>
-  </div>
+  <nx-code></nx-code>
 </template>
 <script>
   export default {
-    data() {
-      return {
-        data: [],
-        value: [1, 4]
-      };
-    },
-    methods: {
-      handleAddData() {
-        let start = Date.now();
-        const data = [];
-        for (let i = 1; i <= 3000; i++) {
-          data.push({
-            key: i,
-            label: `备选项 ${ i }`
-          });
-        }
-        this.data = data;
-        let end = Date.now();
-        console.log(`${(end - start) / 1000}s`);
-      },
-      handleDeleteData() {
-        this.data = [];
-      }
-    },
-  };
+  }
 </script>
